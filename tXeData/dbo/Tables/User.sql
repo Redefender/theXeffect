90898e28b07413b8de8dc9e7c442d8bb77436a57
Ezra Jesalva <ezra.jesalva15@gmail.com>
2019-05-06 10:49:33 -0400

    add user table

---
+++ tXeData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[AuthUserId] NVARCHAR(128) PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [EmailAddress] NVARCHAR(256) NULL, 
+    [CreatedDate] DATETIME2 NULL DEFAULT getutcdate()
+)
