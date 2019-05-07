179e52bf10638c6c33f000765db18175a0262b88
Ezra Jesalva <ezra.jesalva15@gmail.com>
2019-05-07 07:04:35 -0400

    add habit table

---
+++ tXeData/dbo/Tables/Habit.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Habit]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [UserId] NVARCHAR(128) NULL, 
+    CONSTRAINT FK_Habit_ToUser FOREIGN KEY (UserId) REFERENCES [User] ([AuthUserId])
+)
