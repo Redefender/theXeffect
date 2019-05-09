092e3e8e80cc21c879d08d0a5202618d6b998e29
Ezra Jesalva <ezra.jesalva15@gmail.com>
2019-05-09 07:20:41 -0400

    configure axios and make example call

--- tXeClient/src/components/Main.vue
+++ tXeClient/src/components/Main.vue
@@ -1,37 +1,45 @@
-<template>
+    <template>
     <div>
       
         <form v-if="loggedIn" v-on:submit.prevent="onSubmit">
               <h1>Login</h1>
-            <input type="text" placeholder="Username" v-model="name" />
+            <!-- <input type="text" placeholder="Username" v-model="name" />
             <input type="text" placeholder="Password" v-model="password" />
-            <input  type="submit" v-on:click="callLogin"/>
-       
+            <input  type="submit" v-on:click="callLogin"/>-->
+            <input type="button"  value="call Login"  v-on:click="callLogin"/>
+
         </form>
         <div v-else>
             <h1>Welcome!</h1>
         </div>
+        
     </div>
 </template>
 
 <script>
     export default {
         name: 'Login',
-        data:{
-        myData: {}
-        },
+        // data:{
+        // myData: {}
+        // },
         methods: {
             getFormValues() {
                 // console.log(this.name, this.password);
             },
-            callLogin(){
-                alert('password, name', this.password + ' ' + this.name );
+            callLogin : function(){
+                // alert('password, name', this.password + ' ' + this.name );
 
-                axios
-                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                .then(response => { 
-                    myData.push(this.info)
-                });
+                const BaseURI = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+                this.$axios.get(BaseURI)
+                .then((response)=>{
+                    alert(JSON.stringify(response))
+                    // this.myData.push(result.data) 
+                })
+            }
+        },
+        data:function(){
+            return{
+                myData: {}
             }
         },
 
@@ -42,11 +50,7 @@
                
            }
         },
-        watch:{
-            myData: function(){
-                alert(myData);
-            }
-        }
+    
     };
 </script>
 
--- tXeClient/src/main.js
+++ tXeClient/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = true;
 
+Vue.prototype.$http = VueAxios
 new Vue({
-    render: h => h(App)
+    render: h => h(App),
+
 }).$mount('#app');
